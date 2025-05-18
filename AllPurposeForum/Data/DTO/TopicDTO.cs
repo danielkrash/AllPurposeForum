bd60c52a874fef3ca4876d238a82b6b15993821b
Daniel Denisov <danikmass121@gmail.com>
2025-05-18 21:28:39 +0300

    Add TopicDTO class to represent topic data with relevant properties

---
+++ AllPurposeForum/Data/DTO/TopicDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace AllPurposeForum.Data.DTO;
+
+public class TopicDTO
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool Nsfw { get; set; }
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public int PostsCount { get; set; }
+}

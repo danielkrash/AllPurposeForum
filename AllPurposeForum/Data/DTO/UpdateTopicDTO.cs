c09f5bd253f03059b65a9a4f90bba7b7ba43e5a7
Daniel Denisov <danikmass121@gmail.com>
2025-05-18 21:29:07 +0300

    Add UpdateTopicDTO class to represent data for updating topics

---
+++ AllPurposeForum/Data/DTO/UpdateTopicDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AllPurposeForum.Data.DTO;
+
+public class UpdateTopicDTO
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool Nsfw { get; set; }
+}

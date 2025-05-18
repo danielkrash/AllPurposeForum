8110c0f3bb5c7fd89ddb54b243121d0ef139b309
Daniel Denisov <danikmass121@gmail.com>
2025-05-18 21:26:07 +0300

    Add ITopicService interface for managing topics

---
+++ AllPurposeForum/Services/ITopicService.cs
@@ -0,0 +1,14 @@
+﻿using AllPurposeForum.Data.DTO;
+
+namespace AllPurposeForum.Services;
+
+public interface ITopicService
+{
+    public Task<List<TopicDTO>> GetAllTopicsAsync();
+    public Task<TopicDTO> GetTopicByIdAsync(int id);
+    public Task<CreateTopicDTO> CreateTopicAsync(CreateTopicDTO topic);
+    public Task<TopicDTO> UpdateTopicAsync(UpdateTopicDTO topic, int id);
+    public Task<bool> DeleteTopicAsync(int id);
+    public Task<List<TopicDTO>> GetTopicsByUserIdAsync(string userId);
+    public Task<List<TopicDTO>> GetTopicsByPostIdAsync(int postId);
+}

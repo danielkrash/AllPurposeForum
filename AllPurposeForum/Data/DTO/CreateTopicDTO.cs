597a00834c76d035316a1301ef1b840adf660afb
Daniel Denisov <danikmass121@gmail.com>
2025-05-18 21:25:38 +0300

    Add CreateTopicDTO for topic creation

---
+++ AllPurposeForum/Data/DTO/CreateTopicDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AllPurposeForum.Data.DTO;
+
+public class CreateTopicDTO
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string UserId { get; set; }
+    public bool Nsfw { get; set; }
+}
